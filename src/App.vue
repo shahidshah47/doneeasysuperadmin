5ac5a2fcbe265a828105dcfaeb34e40896a85868
mohsinikramvu <mohsinikram1999@gmail.com>
2025-04-15 15:40:30 +0500

    match the design with figma of toast messages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
-import { Toast } from 'primevue';
+import { Avatar, Toast } from 'primevue';
 
 </script>
 
 <template>
   <div class="min-h-screen">
-    <Toast position="top-right" />
+    <Toast position="bottom-right">
+      <template #message="slotProps">
+        <div class="flex flex-col items-start flex-auto">
+          <div class="font-semibold !text-dm-blue text-base">{{ slotProps.message.summary + "!" }}</div>
+          <div class="font-medium !text-gray-500 text-base">{{ slotProps.message.detail }}</div>
+        </div>
+      </template>
+    </Toast>
     <router-view />
   </div>
 </template>
